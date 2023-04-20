76b7ac707d42edd6c48c718c31936573591b0196
rohitsarohamobi <rohitsaroha@mobikasa.com>
2023-04-20 13:38:11 +0530

    api changes

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  res.send('bhai kyun marwa raha')
+  res.send('hello world')
 })
 
 // routes handling
